97d8746aef31e4366d1bb392458a18f08a3f1206
agalkin@ott1-eng.internal <andrei.galkin@gemalto.com>
2019-03-06 17:43:47 -0500

    getting google meta data

--- src/main.go
+++ src/main.go
@@ -3,19 +3,49 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"reflect"
+	"regexp"
 )
 
+type logWriter struct{}
+
 func main() {
+	// fileUrl := "https://golangcode.com/images/avatar.jpg"
 
-	fileUrl := "https://golangcode.com/images/avatar.jpg"
+	// if err := DownloadImage("image.jpg", fileUrl); err != nil {
+	// 	panic(err)
+	// }
 
-	if err := DownloadImage("image.jpg", fileUrl); err != nil {
+	// fmt.Println("Success!")
+
+	url := "https://www.google.com/search?q=c%23+image&rlz=1C1GCEU_enCA826CA826&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjfgeT_pu7gAhVp74MKHZZHB9wQ_AUIDigB&biw=1536&bih=723&dpr=1.25#imgrc=40_i-tVgNtfjAM:"
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
+
+	resp, err := client.Do(req)
+	if err != nil {
 		panic(err)
 	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	imageSource := string(body)
 
-	fmt.Println("Success!")
+	fmt.Println("\n", len(imageSource))
+	fmt.Println(reflect.TypeOf(imageSource))
+
+	r := regexp.MustCompile("<div class=\"rg_meta notranslate\">([\\s\\S]*?)</div>")
+
+	matches := r.FindAllStringSubmatch(imageSource, -1)
+	fmt.Println("GOT : ", len(matches))
+
+	for index, each := range matches {
+		fmt.Println("--------->\n", index, each)
+	}
 }
 
 func DownloadImage(filepath string, url string) error {
