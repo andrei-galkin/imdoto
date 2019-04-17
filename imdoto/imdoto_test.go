2bf15b0edd0b0d7af686014fbb30a45e0e48f167
agalkin <andrei.galkin@gemalto.com>
2019-04-17 13:52:18 -0400

    Tests

--- imdoto/imdoto_test.go
+++ imdoto/imdoto_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestCleanFileName(t *testing.T) {
 
-	testsString := "+testN?a%m#e"
+	testsString := "+testN?a%m#e~"
 	cleanedString := CleanFileName(testsString)
 	if cleanedString != "testName" {
 		t.Errorf("leanedString was incorrect, got: %s, want: %s.", cleanedString, "testName")
