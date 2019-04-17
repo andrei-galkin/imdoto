916414baa278fab22f5bacdbb9cc8d165786cce0
agalkin <andrei.galkin@gemalto.com>
2019-04-17 13:53:33 -0400

    Tests

--- gsearch/gsearch_test.go
+++ gsearch/gsearch_test.go
@@ -2,7 +2,7 @@ package gsearch
 
 import "testing"
 
-func TestImageItemFromJson(t *testing.T) {
+func TestGetImageItemFromJson(t *testing.T) {
 	jsonImage := "{\"cb\": 3, \"cl\": 9, \"clt\": \"n\", \"cr\": 12, \"id\": \"-TtEc9M5pE7LPM:\", \"isu\": \"apple.com\", \"itg\": 0, \"ity\": \"png\", \"oh\": 302, \"ou\": \"https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?201606271147\", \"ow\": 302, \"pt\": \"Buy Apple Pencil - Apple (CA)\", \"rh\": \"apple.com\", \"rid\": \"dQvg6kjka863yM\", \"rt\": 0, \"ru\": \"https://www.apple.com/ca/shop/product/MK0C2AM/A/apple-pencil\", \"s\": \"\", \"sc\": 1, \"st\": \"Apple\", \"th\": 225, \"tu\": \"https://encrypted-tbn0.gstatic.com/images?q\u003dtbn:ANd9GcSYfgh6R22EUz-8EUwh-e68sSEFOHWXeZHuysRW_DPkhahMgdwJ\", \"tw\": 225}"
 	img, err := GetImageItemFromJson(jsonImage)
 
@@ -15,7 +15,7 @@ func TestImageItemFromJson(t *testing.T) {
 	}
 }
 
-func TestImageItemFromJsonEmptyJson(t *testing.T) {
+func TestGetImageItemFromEmptyJson(t *testing.T) {
 	jsonImage := " "
 	img, err := GetImageItemFromJson(jsonImage)
 
